f30f5e18dbd2d835cf8126b9c913ee0ff0efe188
martin-martin <breuss.martin@gmail.com>
2017-08-24 19:42:22 +0200

    feat: add random borders

--- src/MovingBubble.js
+++ src/MovingBubble.js
@@ -6,13 +6,18 @@ class MovingBubble extends Component {
   // add to return() fun for debugging:
   // {JSON.stringify(this.props)}
 
+  state = {
+    borderStylez : ['dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset']
+  }
+
   render() {
     return (
       <div className="all-things">
         {this.props.bubbles.map((bubble) => (
           <div className="bubble" style={{
               color: `${bubble.color}`, // maybe better later backgroundColor instead
-              fontSize: `${bubble.size}em`
+              fontSize: `${bubble.size}em`,
+              borderStyle: `${this.state.borderStylez.sort(function() {return 0.5 - Math.random()})[0]}`
             }}>
             <span>{bubble.title}</span>
           </div>
