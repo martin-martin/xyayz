8a0fb8570279aec5fcd8aa19573f792394d49588
martin-martin <breuss.martin@gmail.com>
2017-08-30 21:29:13 +0200

    fix: add 'key' to iterator

--- src/MovingBubble.js
+++ src/MovingBubble.js
@@ -5,6 +5,9 @@ class MovingBubble extends Component {
 
   // add to return() fun for debugging:
   // {JSON.stringify(this.props)}
+  static propTypes = {
+    bubbles: PropTypes.array.isRequired
+  }
 
   state = {
     borderStylez : ['dotted', 'dashed', 'solid', 'double']
@@ -14,9 +17,10 @@ class MovingBubble extends Component {
     return (
       <div className="all-things">
       {console.log(JSON.stringify(0.5 - Math.random()))}
-        {this.props.bubbles.map((bubble) => (
+        {this.props.bubbles.map((bubble, i) => (
           <div
             className="bubble"
+            key={i}
             style={{
               color: `${bubble.color}`,
               fontSize: `${bubble.size}em`,
