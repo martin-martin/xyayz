3c58d56fdd157ec9fc64196df6b1dc02c6d3f7d0
martin-martin <breuss.martin@gmail.com>
2017-09-07 22:37:45 +0200

    feat: begin func to increase opacity

--- src/ThoughtfulThoughts.js
+++ src/ThoughtfulThoughts.js
@@ -3,52 +3,32 @@ import React, { Component } from 'react'
 class ThoughtfulThoughts extends Component {
 
   state = {
+    opacity: 0.5,
     thoughts:
       [
+        "live and let leave",
         "be who you car",
         "who knows what laf is?"
       ]
   }
 
-  // https://stackoverflow.com/a/2207751
-  appear = (elm, i, step, speed) => {
-    var t_o;
-    //initial opacity
-    i = i || 0;
-    //opacity increment
-    step = step || 5;
-    //time waited between two opacity increments in msec
-    speed = speed || 50;
+  makeAppear = () => {
+    console.log(this.state.opacity)
 
-    t_o = setInterval(function(){
-        //get opacity in decimals
-        var opacity = i / 100;
-        //set the next opacity step
-        i = i + step;
-        if(opacity > 1 || opacity < 0){
-            clearInterval(t_o);
-            //if 1-opaque or 0-transparent, stop
-            return;
-        }
-        //modern browsers
-        elm.style.opacity = opacity;
-        //older IE
-        elm.style.filter = 'alpha(opacity=' + opacity*100 + ')';
-    }, speed);
+    // TODO: currently this causes a "RangeError: Maximum call stack size exceeded"
+
+    // this.setState(prevState => {
+    //   opacity: prevState.opacity + 0.2
+    // })
   }
 
   render() {
     return (
       <div className="thoughtful-thoughts">
-        <div className="current-thought">
-          {this.state.thoughts.map((thought, i) => (
-            <p>{thought}</p>
-          ))}
+        <div className="current-thought" style={{opacity: this.state.opacity}}>
+          {this.state.thoughts.map((thought, i) => (<p>{thought}</p>))}
         </div>
-        <button
-          onClick={this.appear(
-            document.getElementsByClassName('current-thought')[0], 0, 5, 40
-          )}>Think for me</button>
+        <button onClick={this.makeAppear()}>Think for me</button>
       </div>
     )
 
