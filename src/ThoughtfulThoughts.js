aca85cabe08bcf33e5d84c2b635c201d3b1dd763
martin-martin <breuss.martin@gmail.com>
2017-09-08 16:29:24 +0200

    feat: shuffle thoughts (lightheaded)

--- src/ThoughtfulThoughts.js
+++ src/ThoughtfulThoughts.js
@@ -4,6 +4,7 @@ class ThoughtfulThoughts extends Component {
 
   state = {
     opacity: 0,
+    currentThought: "ai!",
     thoughts:
       [
         "live and let leave",
@@ -12,7 +13,38 @@ class ThoughtfulThoughts extends Component {
       ]
   }
 
+  /**
+   * Calls shuffleArray() to mix the thought items in the array,
+   * then updates the state.
+   */
+  componentWillMount = () => {
+    const shuffledThoughts = this.shuffleArray(this.state.thoughts)
+    this.setState((prevState) => ({
+      thoughts: shuffledThoughts
+    }))
+    console.log(this.state.thoughts)
+  }
+
+  /**
+   * Shuffles the items in an array. Code from stackoverflow, thanks!
+   * https://stackoverflow.com/a/38103853/5717580
+   */
+  shuffleArray = (array) => {
+    let i = array.length - 1
+    for (; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      const temp = array[i]
+      array[i] = array[j]
+      array[j] = temp
+    }
+    return array
+  }
+
+  /**
+   * Increases the opacity variable in state by 0.1
+   */
   makeAppear = () => {
+    //TODO: include fun stuff like: when clicked 50 times text turns red : )
     if(this.state.opacity < 1) {
       this.setState((prevState) => ({
           opacity: prevState.opacity + 0.1
@@ -23,10 +55,11 @@ class ThoughtfulThoughts extends Component {
   render() {
     return (
       <div className="thoughtful-thoughts">
-        <div className="current-thought" style={{opacity: this.state.opacity}}>
-          {this.state.thoughts.map((thought, i) => (<p key={i}>{thought}</p>))}
-        </div>
+        <p className="current-thought" style={{opacity: this.state.opacity}}>
+          {this.state.currentThought}
+        </p>
         <button onClick={this.makeAppear}>Focus!</button>
+        <button onClick={this.seedNextThought}>Think...</button>
       </div>
     )
 
