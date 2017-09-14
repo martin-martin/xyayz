dfa211fd2e87036c0cda4219a43dbc9d853e304b
martin-martin <breuss.martin@gmail.com>
2017-09-14 20:27:40 +0200

    feat: add Routes, separate pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react';
-import MovingBubble from './MovingBubble';
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import MovingBubble from './MovingBubble'
 import TextPost from './TextPost'
 import ThoughtfulThoughts from './ThoughtfulThoughts'
-import './App.css';
+import './App.css'
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <ThoughtfulThoughts/>
-        <MovingBubble/>
-        <TextPost/>
+        <Route path="/" render={() => (
+          <MovingBubble/>
+        )}/>
+        <Route path="/notes" render={() => (
+          <ThoughtfulThoughts/>
+        )}/>
+        <Route path="/me" render={() => (
+          <TextPost/>
+        )}/>
       </div>
     );
   }
