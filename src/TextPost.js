24266ad29cb5387af2c9e8aa58294cf10aa99a97
martin-martin <breuss.martin@gmail.com>
2017-10-08 14:56:28 +0200

    feat: add char counts to notes

--- src/TextPost.js
+++ src/TextPost.js
@@ -7,13 +7,18 @@ class TextPost extends Component {
   state = notes
 
   render() {
+    // how many characters a newline takes in the data
+    var newlineOffset = 12
+
     return (
         <div className="notebook">
           <NavBack/>
           <div className="padding"></div>
         {this.state.notes.map((note, i) => (
           <details className="text-note" key={i}>
-            <summary className="note-title">{note.title}</summary>
+            <summary className="note-title">{note.title +
+               " (+" + (note.body.length - note.title.length - newlineOffset) +
+               "c)"}</summary>
             <div className="note-body" dangerouslySetInnerHTML={{__html: note.body}}></div>
           </details>
         ))}
