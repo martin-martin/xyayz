2f287fd2b6e97306e9437ac557ae3603dd608d28
martin-martin <breuss.martin@gmail.com>
2017-10-08 14:38:24 +0200

    feat: toggle notes interactively

--- src/TextPost.js
+++ src/TextPost.js
@@ -12,10 +12,10 @@ class TextPost extends Component {
           <NavBack/>
           <div className="padding"></div>
         {this.state.notes.map((note, i) => (
-          <div className="text-note" key={i}>
-            <h1>{note.title}</h1>
-            <div dangerouslySetInnerHTML={{__html: note.body}}></div>
-          </div>
+          <details className="text-note" key={i}>
+            <summary className="note-title">{note.title}</summary>
+            <div className="note-body" dangerouslySetInnerHTML={{__html: note.body}}></div>
+          </details>
         ))}
         </div>
     )
